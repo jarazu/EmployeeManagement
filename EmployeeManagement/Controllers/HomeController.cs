4a44d7e6b4fde1c51c689b385723124dff3342f8
Razu <jahangir8988@gmail.com>
2021-12-20 23:14:03 +0600

    ok

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,5 +20,15 @@ namespace EmployeeManagement.Controllers
         {
             return _employeeRepository.GetEmployee(1).Name;
         }
+
+        public ViewResult Details()
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel() {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsViewModel);
+        }
     }
 }
+
